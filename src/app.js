a039297da92095b4c675f68f31a1d07f88b7fedf
Erin Gurley <erinmgurley@gmail.com>
2019-04-23 12:00:51 -0700

    Link app.js to index.html and get the html elements with DOM selector.

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const submitButton = document.getElementById('submit');
+
+
+const jacksAnswer = document.getElementById('jacks-favorite-thing');
+const denisesAnswer = document.getElementById('denises-favorite-thing');
+const erinsAnswer = document.getElementById('erins-favorite-thing');
+const leighAnnAnswer = document.getElementById('leigh-anns-favorite-thing');
+console.log(submitButton, jacksAnswer, denisesAnswer, erinsAnswer, leighAnnAnswer);
