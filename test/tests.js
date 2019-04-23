3ff7ae189a88832d60c8eed2a2c825559b43b2ea
Erin Gurley <erinmgurley@gmail.com>
2019-04-23 13:23:58 -0700

    created test for all failing and wrote function to pass the test

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,23 @@
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+function quizScore(jacksAnswer, denisesAnswer, erinsAnswer, leighAnnsAnswer) {
+    let score = 0;
+    return 0;
+}
+
+test('All entrys are wrong expecting 0', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const jacksAnswer = "lsjkdf";
+    const denisesAnswer = "cat";
+    const erinsAnswer = "dog";
+    const leighAnnsAnswer = "mouse";
+    const expected = 0;
 
     //Act 
     // Call the function you're testing and set the result to a const
+    const score = quizScore(jacksAnswer, denisesAnswer, erinsAnswer, leighAnnsAnswer);
 
     //Assert
-    assert.equal(true, false);
+    assert.equal(score, expected);
 });
