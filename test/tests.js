e54071e589c54a6b1fecd56ed086d7b2b646dce4
Erin Gurley <erinmgurley@gmail.com>
2019-04-23 13:38:26 -0700

    Add test for second favorite and add or to if statements to test for second answer.

--- test/tests.js
+++ test/tests.js
@@ -3,19 +3,19 @@ const test = QUnit.test;
 function quizScore(jacksAnswer, denisesAnswer, erinsAnswer, leighAnnsAnswer) {
     let score = 0;
 
-    if(jacksAnswer === "waterskiing"){
+    if(jacksAnswer === "waterskiing" || jacksAnswer === "lacrosse"){
         score++;
     }
 
-    if(denisesAnswer === "tacos"){
+    if(denisesAnswer === "tacos" || denisesAnswer === "running"){
         score++;
     }
 
-    if(erinsAnswer === "poetry"){
+    if(erinsAnswer === "poetry" || erinsAnswer === "traveling"){
         score++;
     }
 
-    if(leighAnnsAnswer === "eurovision"){
+    if(leighAnnsAnswer === "eurovision" || leighAnnsAnswer === "puppies"){
         score++;
     }
 
@@ -39,13 +39,30 @@ test('All entrys are wrong expecting 0', function(assert) {
     assert.equal(score, expected);
 });
 
-test('All entrys are correct expecting 4', function(assert) {
+test('All entrys are correct as top favorite expecting 4', function(assert) {
     //Arrange
     // Set up your parameters and expectations
     const jacksAnswer = "waterskiing";
     const denisesAnswer = "tacos";
     const erinsAnswer = "poetry";
     const leighAnnsAnswer = "eurovision";
+    const expected = 4;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const score = quizScore(jacksAnswer, denisesAnswer, erinsAnswer, leighAnnsAnswer);
+
+    //Assert
+    assert.equal(score, expected);
+});
+
+test('All entrys are correct as second favorite expecting 4', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const jacksAnswer = "lacrosse";
+    const denisesAnswer = "running";
+    const erinsAnswer = "traveling";
+    const leighAnnsAnswer = "puppies";
     const expected = 4;
 
     //Act 
