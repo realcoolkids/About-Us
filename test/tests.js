9c7948971e2700ce00e3a17d4461611e47cfda90
Erin Gurley <erinmgurley@gmail.com>
2019-04-23 14:05:49 -0700

    placed quiz score function in its own file and import into tests

--- test/tests.js
+++ test/tests.js
@@ -1,31 +1,5 @@
+import quizScore from "../src/quizScore.js"
 const test = QUnit.test;
-
-function quizScore(jacksAnswer, denisesAnswer, erinsAnswer, leighAnnsAnswer) {
-    let score = 0;
-
-    jacksAnswer = jacksAnswer.toLowerCase();
-    denisesAnswer = denisesAnswer.toLowerCase();
-    erinsAnswer = erinsAnswer.toLowerCase();
-    leighAnnsAnswer = leighAnnsAnswer.toLowerCase();
-
-    if(jacksAnswer === "waterskiing" || jacksAnswer === "lacrosse" || jacksAnswer === "hockey"){
-        score++;
-    }
-
-    if(denisesAnswer === "tacos" || denisesAnswer === "running" || denisesAnswer === "swimming"){
-        score++;
-    }
-
-    if(erinsAnswer === "poetry" || erinsAnswer === "traveling" || erinsAnswer === "nature"){
-        score++;
-    }
-
-    if(leighAnnsAnswer === "eurovision" || leighAnnsAnswer === "puppies" || leighAnnsAnswer === "podcasts"){
-        score++;
-    }
-
-    return score;
-}
 
 test('All entrys are wrong expecting 0', function(assert) {
     //Arrange
