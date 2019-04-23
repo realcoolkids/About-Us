bd4709986d3d743f9b1c3b47fffb1de7f719ae69
Erin Gurley <erinmgurley@gmail.com>
2019-04-23 14:53:27 -0700

    added good and bad coloring for score results

--- src/app.js
+++ src/app.js
@@ -12,14 +12,24 @@ submitButton.addEventListener("click",() => {
     const denisesAnswer = denisesInput.value;
     const erinsAnswer = erinsInput.value;
     const leighAnnsAnswer = leighAnnsInput.value;
+
+    let score = 0;
     
     let scoreEl = document.getElementById("score")
     
-    if(jacksAnswer.trim() === "" || denisesAnswer.trim() === "" || erinsAnswer.trim() === "" || leighAnnsAnswer.trim() === ""){
+    if(jacksAnswer.trim() === "" || denisesAnswer.trim() === "" || erinsAnswer.trim() === "" || leighAnnsAnswer.trim() === "") {
         scoreEl.textContent = "Please fill out all options.";
     } else {
-        const score = quizScore(jacksAnswer, denisesAnswer, erinsAnswer, leighAnnsAnswer);
+        score = quizScore(jacksAnswer, denisesAnswer, erinsAnswer, leighAnnsAnswer);
+        if(score <= 2){
+            scoreEl.classList.add("bad");
+            scoreEl.classList.remove("good");
+        } else {
+            scoreEl.classList.remove("bad");
+            scoreEl.classList.add("good");
+        }
 
+    
         scoreEl.textContent = "Your score: " + score;
     }
 }) 
