dc9eb47f088cb90f0134a1c83309208fc060b341
Erin Gurley <erinmgurley@gmail.com>
2019-04-23 13:32:42 -0700

    created new test for all passing, and added if statements to test

--- test/tests.js
+++ test/tests.js
@@ -2,7 +2,24 @@ const test = QUnit.test;
 
 function quizScore(jacksAnswer, denisesAnswer, erinsAnswer, leighAnnsAnswer) {
     let score = 0;
-    return 0;
+
+    if(jacksAnswer === "waterskiing"){
+        score++;
+    }
+
+    if(denisesAnswer === "tacos"){
+        score++;
+    }
+
+    if(erinsAnswer === "poetry"){
+        score++;
+    }
+
+    if(leighAnnsAnswer === "eurovision"){
+        score++;
+    }
+
+    return score;
 }
 
 test('All entrys are wrong expecting 0', function(assert) {
@@ -13,6 +30,23 @@ test('All entrys are wrong expecting 0', function(assert) {
     const erinsAnswer = "dog";
     const leighAnnsAnswer = "mouse";
     const expected = 0;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const score = quizScore(jacksAnswer, denisesAnswer, erinsAnswer, leighAnnsAnswer);
+
+    //Assert
+    assert.equal(score, expected);
+});
+
+test('All entrys are correct expecting 4', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const jacksAnswer = "waterskiing";
+    const denisesAnswer = "tacos";
+    const erinsAnswer = "poetry";
+    const leighAnnsAnswer = "eurovision";
+    const expected = 4;
 
     //Act 
     // Call the function you're testing and set the result to a const
