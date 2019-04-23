01fb3db7b90c1958dfc4dcae23b856cdea35ae93
Erin Gurley <erinmgurley@gmail.com>
2019-04-23 13:42:08 -0700

    add to function to test for third answer

--- test/tests.js
+++ test/tests.js
@@ -3,19 +3,19 @@ const test = QUnit.test;
 function quizScore(jacksAnswer, denisesAnswer, erinsAnswer, leighAnnsAnswer) {
     let score = 0;
 
-    if(jacksAnswer === "waterskiing" || jacksAnswer === "lacrosse"){
+    if(jacksAnswer === "waterskiing" || jacksAnswer === "lacrosse" || jacksAnswer === "hockey"){
         score++;
     }
 
-    if(denisesAnswer === "tacos" || denisesAnswer === "running"){
+    if(denisesAnswer === "tacos" || denisesAnswer === "running" || denisesAnswer === "swimming"){
         score++;
     }
 
-    if(erinsAnswer === "poetry" || erinsAnswer === "traveling"){
+    if(erinsAnswer === "poetry" || erinsAnswer === "traveling" || erinsAnswer === "nature"){
         score++;
     }
 
-    if(leighAnnsAnswer === "eurovision" || leighAnnsAnswer === "puppies"){
+    if(leighAnnsAnswer === "eurovision" || leighAnnsAnswer === "puppies" || leighAnnsAnswer === "podcasts"){
         score++;
     }
 
@@ -63,6 +63,23 @@ test('All entrys are correct as second favorite expecting 4', function(assert) {
     const denisesAnswer = "running";
     const erinsAnswer = "traveling";
     const leighAnnsAnswer = "puppies";
+    const expected = 4;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const score = quizScore(jacksAnswer, denisesAnswer, erinsAnswer, leighAnnsAnswer);
+
+    //Assert
+    assert.equal(score, expected);
+});
+
+test('All entrys are correct as third favorite expecting 4', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const jacksAnswer = "hockey";
+    const denisesAnswer = "swimming";
+    const erinsAnswer = "nature";
+    const leighAnnsAnswer = "podcasts";
     const expected = 4;
 
     //Act 
